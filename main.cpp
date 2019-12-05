05b2b03695d9424ade73c98e1cbb83118db10e1f
kamikoto <kamikoto.sudzuki@gmail.com>
2019-12-06 01:50:50 +0200

    has add writing in file

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 
 static bool find_filename(int ac, char** av, std::string& filename)
 {
-	std::string	expression = "(-file=)(\\w+)";
+	std::string	expression = "(-file=)(\\w+)(.)?(\\w*)";
 	std::regex flag(expression);
 	std::cmatch info;
 
 	for (int i = 1; i < ac; i++)
 		if (std::regex_match(av[i], info, flag))
 		{
-			filename = info.str(2);
+			filename = info.str(2) + info.str(3) + info.str(4);
 			return (true);
 		}
 	return (false);
